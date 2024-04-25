3fc1d1849e28505f351575a4f63ce70efa1802e2
carlosperalesa <carlosperalesa@gmail.com>
2024-04-25 03:38:59 -0400

    prueba

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -133,6 +133,7 @@ MEDIA_ROOT = os.path.join(
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
