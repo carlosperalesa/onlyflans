422071426d68b8c16457b9861968643e2c0e2072
carlosperalesa <carlosperalesa@gmail.com>
2024-04-25 20:57:05 -0400

    hito 5
    ultima actualizacion

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -121,12 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(
-    BASE_DIR, 'onlyflans_djweb/templates/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'onlyflans_djweb/templates/static')]
+
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(
-    BASE_DIR, 'onlyflans_djweb/media/img/productos')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'onlyflans_djweb/media/')
 
 
 # Default primary key field type
